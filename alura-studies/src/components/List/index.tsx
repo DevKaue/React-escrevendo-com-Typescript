f0751768765ee4a49d69377853d353a50dc31b4e
Kaue Sabino <kaue.sabino@modalgr.com.br>
2023-12-22 10:11:19 -0300

    fix: Refatorando a Lista

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,37 +1,40 @@
 import React from "react";
-import style from  './List.module.scss';
+import style from "./List.module.scss";
+import Item from "./item";
 
-function List() 
-{
+function List() {
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "JavaScript",
+      time: "01:00:00",
+    },
+    {
+      task: "TypeScript",
+      time: "03:00:00",
+    },
+  ];
 
-    const tasks = [
-        {
-            task: 'React',
-            time: '02:00:00'
-        },
-        {
-            task: 'JavaScript',
-            time: '01:00:00'
-        },
-        {
-            task: 'TypeScript',
-            time: '03:00:00'
-        }
-    ]
-
-    return(
-        <aside className={style.listaTarefas}>
-            <h2> Estudos do dia </h2>
-            <ul>
-                {tasks.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3> {item.task} </h3>
-                        <span> {item.time} </span>
-                    </li>
-                ))}
-            </ul>
-        </aside>
-    )
+  return (
+    <aside className={style.listaTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tasks.map((item, index) => (
+          <Item 
+           key={index}
+           {...item} //utilizando todos os atributos de props dentro do componente
+          />
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default List;
+
+/* Uma dar maneiras de usar as props do Item
+<Item task={item.task} time={item.time} />;
+*/
