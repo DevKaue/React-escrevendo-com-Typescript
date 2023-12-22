6859b7846843371fd7c0907f0a94d302e92258fe
Kaue Sabino <kaue.sabino@modalgr.com.br>
2023-12-22 09:48:17 -0300

    feat: conhecendo props do React

--- alura-studies/src/components/button/index.tsx
+++ alura-studies/src/components/button/index.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import style from './Button.module.scss';
 
-class Button extends React.Component {
-    render() {
+class Button extends React.Component<{text: string}> {
+    render() 
+    {
         return(
-            <button className={style.botao}>Botao</button>
+            <button className={style.botao}>{this.props.text}</button>
         )
     }
 }
 
 export default Button;
+
+
+/*
+Uma das formas de Utilizar props em componente
+
+class Button extends React.Component {
+    render() 
+    {
+        return(
+            <button className={style.botao}>{this.props.children}</button>
+        )
+    }
+}
+
+*/
