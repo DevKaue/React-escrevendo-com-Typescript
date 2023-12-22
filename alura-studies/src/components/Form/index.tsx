6534a7151e0767303fd663f9fb889eb17022eb8a
Kaue Sabino <kaue.sabino@modalgr.com.br>
2023-12-22 09:13:59 -0300

    feat: Adicionando o css Modules

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "../Button";
-import './style.scss';
+import style from './Form.module.scss';
 
 class Form extends React.Component {
     render() {
         return(
-            <form className="novaTarefa">
-                <div className="inputContainer">
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo Estudo</label>
                     <input type="text"
                     name="tarefa"
@@ -14,7 +14,7 @@ class Form extends React.Component {
                     placeholder="O que você quer estudar?"
                     required/>
                 </div>
-                <div className="inputContainer">
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
                     <input type ="time"
                     step="1"
