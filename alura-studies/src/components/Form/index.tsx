463b699055f3079a83188ae9402b429da66cbab4
Kaue Sabino <kaue.sabino@modalgr.com.br>
2023-12-22 13:51:34 -0300

    feat: realizando controle do formulário

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -3,13 +3,27 @@ import Button from "../Button";
 import style from './Form.module.scss';
 
 class Form extends React.Component {
+
+    state = {
+        task: "",
+        time: "00:00"
+    };
+
+    addTask(event: React.FormEvent<HTMLFormElement>)
+    {
+        event?.preventDefault();
+        console.log('state: ', this.state);
+    }
+
     render() {
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.addTask}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo Estudo</label>
                     <input type="text"
                     name="tarefa"
+                    value={this.state.task}
+                    onChange={event => this.setState({ ...this.state, task: event.target.value })} //pegando o valor do input e armazenando
                     id="tarefa"
                     placeholder="O que você quer estudar?"
                     required/>
@@ -19,6 +33,8 @@ class Form extends React.Component {
                     <input type ="time"
                     step="1"
                     name="tempo"
+                    value={this.state.time}
+                    onChange={event => this.setState({ ...this.state, time: event.target.value })} //pegando o valor do input e armazenando
                     id="tempo"
                     min="00:00:00"
                     max="01:30:00"
