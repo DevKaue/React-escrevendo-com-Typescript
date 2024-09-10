a7cfd212fdf781d7edec9622f0e818def93d15f0
Kauê Wendt Sabino <kaue.sabino@modalgr.com.br>
2024-09-10 14:07:32 -0300

    feat: tempo para segundos

--- alura-studies/src/components/Timer/index.tsx
+++ alura-studies/src/components/Timer/index.tsx
@@ -1,8 +1,10 @@
+import { TimeToSeconds } from "../../common/utils/date";
 import Button from "../button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
 export default function Timer() {
+  console.log("Conversao: ", TimeToSeconds("01:01:01"));
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
