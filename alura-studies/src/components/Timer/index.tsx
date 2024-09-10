4dbbce4bb389bd3a29434310e69b6851f7277b0c
Kauê Wendt Sabino <kaue.sabino@modalgr.com.br>
2024-09-10 14:24:05 -0300

    feat: start no cronometro

--- alura-studies/src/components/Timer/index.tsx
+++ alura-studies/src/components/Timer/index.tsx
@@ -1,13 +1,24 @@
-import { TimeToSeconds } from "../../common/utils/date";
+import { useState } from "react";
+import { TimeToSeconds } from "../../common/utils/time";
+import ITasks from "../../types/tasks";
 import Button from "../button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
-  console.log("Conversao: ", TimeToSeconds("01:01:01"));
+interface Props {
+  selected: ITasks | undefined;
+}
+
+export default function Timer({ selected }: Props) {
+  const [time, setTime] = useState<number>();
+  if (selected?.time) {
+    setTime(TimeToSeconds(selected.time));
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {time}
       <div className={style.relogioWrapper}>
         <Clock />
       </div>
